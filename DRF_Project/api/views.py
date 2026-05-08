e0410c63055a2b1ba8a5ec566e5cdf2286b33a1c
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-09 01:19:22 +0600

    Manually Serializing the object by converting them into list

--- DRF_Project/api/views.py
+++ DRF_Project/api/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 
 # Create your views here.
 
 def studentsView(request):
-    students ={
-        'id' : 1,
-        'name' : 'Romman',
-        'Department' : 'CSE',
-        'Faculty' : 'FSIT'
-    }
+    students =Student.objects.all()
+    students_list = list(students.values())         #manually serializing the data by convert them into list .
 
-    return JsonResponse(students)
+    return JsonResponse(students_list, safe=False)       #We need use safe parameter in JsonResponse in order to allow any non-dict object to serialize. 
