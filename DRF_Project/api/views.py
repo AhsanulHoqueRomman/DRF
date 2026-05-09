8aef9ed9399326a56dea28b64ce58f18ffa64c85
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-10 00:08:27 +0600

    Serializing Objects using django rest_framework post method

--- DRF_Project/api/views.py
+++ DRF_Project/api/views.py
@@ -18,10 +18,18 @@ from rest_framework.decorators import api_view
 
 
 #Serializing Objects using django rest_framework serializers.--Industry standard.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentsView(request):
     if request.method == 'GET':
         #Get all the data of Student table
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer  = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
