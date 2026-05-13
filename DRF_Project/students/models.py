ba35c415d6caaed7db8b4ba1d6f8efe33a7c4647
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-13 23:58:11 +0600

    adding update

--- DRF_Project/students/models.py
+++ DRF_Project/students/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Student(models.Model):
     student_id = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    branch = models.CharField(max_length=30)
+    department = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
