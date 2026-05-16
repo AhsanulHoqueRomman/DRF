4a17e3fff69f05f5ea37bf1b09c530438730f2e8
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-16 18:32:43 +0600

    Implementing Custom Filter by pk id through rangefilter

--- DRF_Project/employees/filters.py
+++ DRF_Project/employees/filters.py
@@ -6,7 +6,7 @@ from .models import Employee
 class EmpployeeFilter(django_filters.FilterSet):                    #FilterSet have all the functionalities of handling filters.
     designation =  django_filters.CharFilter(field_name='designation', lookup_expr='icontains')     #icontains return result even though there is only one word matches
     emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
-
+    id = django_filters.RangeFilter(field_name='id')
 
     class Meta:
         model = Employee
