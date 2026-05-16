55bf15341a74dc8784acc0a75d39fb966a14f098
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-17 01:58:40 +0600

    Implementing filter for emp_id range by customiizing function in filter class

--- DRF_Project/employees/filters.py
+++ DRF_Project/employees/filters.py
@@ -7,18 +7,18 @@ class EmployeeFilter(django_filters.FilterSet):                    #FilterSet ha
     designation =  django_filters.CharFilter(field_name='designation', lookup_expr='icontains')     #icontains return result even though there is only one word matches
     emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
     # id = django_filters.RangeFilter(field_name='id')        #RangeFilter only works on only integer.emp_id is not integer.
-    # id_min = django_filters.CharFilter(method='filter_by_id_range', label='From EMP ID')
-    # id_max = django_filters.CharFilter(method='filter_by_id_range', label='To EMP ID')
-    emp_id = django_filters.CharFilter(field_name='emp_id',lookup_expr='icontains')             #If we want to filter just one emp_id
+    id_min = django_filters.CharFilter(method='filter_by_id_range', label='From EMP ID')        #If we want to filter employee within a range of emp_id
+    id_max = django_filters.CharFilter(method='filter_by_id_range', label='To EMP ID')
+    # emp_id = django_filters.CharFilter(field_name='emp_id',lookup_expr='icontains')             #If we want to filter just one emp_id
 
 
     class Meta:
         model = Employee
-        fields = ['designation', 'emp_name', 'emp_id']
+        fields = ['designation', 'emp_name', 'id_min', 'id_max']
 
-    # def filter_by_id_range(self, queryset, name, value):
-    #     if name == 'id_min':
-    #         return queryset.filter(emp_id__gte = value)
-    #     if name == 'id_max':
-    #         return queryset.filter(emp_id__lte = value)
-    #     return queryset
+    def filter_by_id_range(self, queryset, name, value):
+        if name == 'id_min':
+            return queryset.filter(emp_id__gte = value)
+        if name == 'id_max':
+            return queryset.filter(emp_id__lte = value)
+        return queryset
